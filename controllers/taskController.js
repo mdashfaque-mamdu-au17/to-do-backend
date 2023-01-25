48be063efcda9d36959c366af9eda985096bcfd2
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-25 22:38:18 +0530

    all crud operations completed

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,8 +1,15 @@
 const Task = require('../models/Task');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, NotFoundError } = require('../errors');
 
-const getAllTasks = (req, res) => {
-  res.send('get all tasks');
+const getAllTasks = async (req, res) => {
+  const { completed } = req.query;
+  const queryObject = {};
+  if (completed) {
+    queryObject.completed = completed;
+  }
+  const tasks = await Task.find(queryObject).sort('-createdAt');
+  res.status(StatusCodes.OK).json({ tasks });
 };
 
 const createTask = async (req, res) => {
@@ -10,15 +17,44 @@ const createTask = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ task });
 };
 
-const updateTask = (req, res) => {
-  res.send('update task');
+const updateTask = async (req, res) => {
+  const {
+    body: { name, completed },
+    params: { id: taskId },
+  } = req;
+
+  if (name === '' || completed === '' || !name || !completed) {
+    throw new BadRequestError(
+      'Task name or completed fields cannot be empty'
+    );
+  }
+  const task = await Task.findByIdAndUpdate(
+    { _id: taskId },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!task) {
+    throw new NotFoundError(`No task with the id ${taskId}`);
+  }
+  res.status(StatusCodes.OK).json({ task });
 };
 
-const deleteTask = (req, res) => {
-  res.send('delete task');
+const deleteTask = async (req, res) => {
+  const { id: taskId } = req.params;
+  const task = await Task.findByIdAndDelete({ _id: taskId });
+
+  if (!task) {
+    throw new NotFoundError(`No task with the id ${taskId}`);
+  }
+  res.status(StatusCodes.OK).send();
 };
 
-const clearCompleted = (req, res) => {
+const clearCompleted = async (req, res) => {
+  await Task.deleteMany({ completed: true });
   res.send('clear all completed tasks');
 };
 
