93b26ba5e86864fdfb2d54a11393bfa7d0c7f497
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-25 22:38:02 +0530

    timestamps added

--- models/Task.js
+++ models/Task.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
-const TaskSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please provide task name'],
-    trim: true,
-    minlength: [1, 'Name cannot be empty'],
+const TaskSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide task name'],
+      trim: true,
+      minlength: [1, 'Name cannot be empty'],
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
   },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Task', TaskSchema);
